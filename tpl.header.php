965c09440c6634df95e3c47bd03afb84eeb06460
Rudie Dirkx <github@hotblocks.nl>
2013-10-13 23:09:30 +0200

    wtf MAC line endings!?

--- tpl.header.php
+++ tpl.header.php
@@ -1,1 +1,64 @@
-<style>html {	background-color: #eee;}table {	border-collapse: collapse;}td, th {	border: solid 1px #bbb;	padding: 6px;}th {	text-align: left;}thead td {	font-size: 120%;	text-align: center;	font-weight: bold;}.c {	text-align: center;}tr.dir-in {	background-color: #dfd;}tr.dir-out {	background-color: #fdd;}td.amount {	font-family: monospace;	text-align: right;}tr.dir-out td.category.empty {	background: #faa;}select:focus {	outline: solid 4px black;}</style><p>	<a href="index.php">Overview</a>	|	<a href="parties.php">Parties</a>	|	<a href="categories.php">Categories</a>	|	<a href="import-ing.php">Import ING</a></p>
+<!doctype html>
+<html>
+
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=1200, initial-scale=0.2" />
+<style>
+html {
+	background-color: #eee;
+}
+
+table {
+	border-collapse: collapse;
+}
+td, th {
+	border: solid 1px #bbb;
+	padding: 6px;
+}
+th {
+	text-align: left;
+}
+thead td {
+	font-size: 120%;
+	text-align: center;
+	font-weight: bold;
+}
+
+.c {
+	text-align: center;
+}
+
+tr.dir-in {
+	background-color: #dfd;
+}
+tr.dir-out {
+	background-color: #fdd;
+}
+
+td.amount {
+	font-family: monospace;
+	text-align: right;
+}
+
+tr.dir-out td.category.empty {
+	background: #faa;
+}
+
+select:focus {
+	outline: solid 4px black;
+}
+</style>
+</head>
+
+<body>
+
+<p>
+	<a href="index.php">Overview</a>
+	|
+	<a href="parties.php">Parties</a>
+	|
+	<a href="categories.php">Categories</a>
+	|
+	<a href="import-ing.php">Import ING</a>
+</p>
