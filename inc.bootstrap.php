8f04ee7355945f14d505193a456868217bc2a38c
Rudie Dirkx <github@hotblocks.nl>
2017-12-26 17:35:35 +0100

    +composer

--- inc.bootstrap.php
+++ inc.bootstrap.php
@@ -2,23 +2,16 @@
 
 require 'env.php';
 
-
-
-require 'inc.functions.php';
+require __DIR__ . '/vendor/autoload.php';
 do_auth();
 
-
-
 // Connect to db
-require WHERE_DB_GENERIC_AT . '/db_sqlite.php'; // https://github.com/rudiedirkx/db_generic
 $db = db_sqlite::open(array('database' => __DIR__ . '/db/moneys.sqlite3'));
-
 if ( !$db ) {
 	exit("<p>No db...</p>");
 }
 
 db_generic_model::$_db = $db;
-require 'inc.models.php';
 
 // Verify db schema
 $schema = require 'inc.schema.php';
@@ -27,4 +20,3 @@ $schema = require 'inc.schema.php';
 // Start UTF-8 everywhere, always
 mb_internal_encoding('utf-8');
 header('Content-type: text/html; charset=utf-8');
-
